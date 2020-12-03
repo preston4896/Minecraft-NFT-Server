dcc4485666797c93c046bdaf62f0ece0c01bde9c
Preston Ong <preston4896@me.com>
2020-12-03 13:32:35 -0800

    Solidity compiled successfully

--- smart-contracts/contracts/DeFi.sol
+++ smart-contracts/contracts/DeFi.sol
@@ -2,9 +2,10 @@
 
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155Holder.sol";
+// import "@openzeppelin/contracts/token/ERC1155/ERC1155Holder.sol"; // uncomment this to implement.
+import "./Tokens.sol";
 
-contract DeFi is ERC1155Reciever{
+contract DeFi is Tokens {
   uint256 trade_ids;
 
   // Trade data struct 
@@ -46,11 +47,11 @@ contract DeFi is ERC1155Reciever{
 
   function openTrade(uint256 nft_id, address borrower, uint256 borrowing_amount, uint256 apy) public returns (uint256) {
     // Check if the borrower has atleast one of NFT
-    require(balanceOf(borrower, uid) >= 1);
+    require(balanceOf(borrower, nft_id) >= 1);
     
     // Create a trade with the current avaiable trade id
     uint256 trade_id = trade_ids;
-    trades[trade_id] = Trade(nft_id, 0x0, borrower, borrowing_amount, apy, 0, 0, State.OPEN);
+    trades[trade_id] = Trade(nft_id, address(0x0), borrower, borrowing_amount, apy, 0, 0, State.OPEN);
     trade_ids++; // Increase the trade id by 1 for the next trade
 
     // Emit that a trade has been opened
@@ -58,7 +59,7 @@ contract DeFi is ERC1155Reciever{
     return trade_id;
   }
 
-  function lendToTrade(uint256 trade_id) public returns bool {
+  function lendToTrade(uint256 trade_id) public returns (bool) {
     // Check if the trade is still open
     require(trades[trade_id].state == State.OPEN);
     // Check if the borrower still has the NFT
@@ -72,16 +73,16 @@ contract DeFi is ERC1155Reciever{
     trades[trade_id].start_time = now;
 
     // Transfer the NFT from the borrower to this contract
-    safeTransfer(trades[trade_id].borrower, address(this), trades[trade_id].nft_id, 1, "0x0");
+    safeTransferFrom(trades[trade_id].borrower, address(this), trades[trade_id].nft_id, 1, "0x0");
     // Transfer the Fungible tokens from the lender to this contract
-    safeTransfer(trades[trade_id].lender, address(this), 0, trades[trade_id].amount, "0x0");
+    safeTransferFrom(trades[trade_id].lender, address(this), 0, trades[trade_id].borrowing_amount, "0x0");
 
     // Emit that the trade has been financed
     emit TradeFinanced(trade_id);
     return true;
   }
 
-  function payInterest(uint256 trade_id, uint256 amount) public returns bool {
+  function payInterest(uint256 trade_id, uint256 amount) public returns (bool) {
     // Make sure the state of the trade is FINANCED
     require(trades[trade_id].state == State.FINANCED);
     // Make sure the caller is the trade's borrower
@@ -89,21 +90,21 @@ contract DeFi is ERC1155Reciever{
     // Make sure the borrower has enough funds
     require(balanceOf(msg.sender, 0) >= amount);
 
-    trades[trade_id].amount_paid_back += amount
-    safeTransfer(msg.sender, address(this), 0, amount, "0x0");
-    emit InterestPaid(trade_id, amount);
+    trades[trade_id].paid_back_amount += amount;
+    safeTransferFrom(msg.sender, address(this), 0, amount, "0x0");
+    emit PaidInterest(trade_id, amount);
 
     // Only if the amount paid back is more than the principal could the transaction be complete
     // Thus we check only if that is the case
-    if (trades[trade_id].amount_paid_back >= trades[trade_id].amount_borrowed) {
+    if (trades[trade_id].paid_back_amount >= trades[trade_id].borrowing_amount) {
       uint256 seconds_since_start = now - trades[trade_id].start_time;
       // Formula for Continous compound interest
       // https://www.investopedia.com/terms/c/continuouscompounding.asp#:~:text=Calculating%20the%20limit%20of%20this,mathematical%20constant%20approximated%20as%202.7183.
-      uint256 amount_to_be_paid = trades[trade_id].amount_borrowed * (2.7183 ** ((seconds_since_start/31556952) 
+      uint256 amount_to_be_paid = trades[trade_id].borrowing_amount * (uint256(3) ** ((seconds_since_start/31556952) 
         * trades[trade_id].apy));
       
 
-      if (trades[trade_id].amount_paid_back >= amount_to_be_paid) {
+      if (trades[trade_id].paid_back_amount >= amount_to_be_paid) {
         completeTrade(trade_id);
       }
     }
@@ -111,14 +112,14 @@ contract DeFi is ERC1155Reciever{
     return true;
   }
 
-  function completeTrade(uint256 trade_id) private returns bool {
+  function completeTrade(uint256 trade_id) private returns (bool) {
     // Makes sure the contract still has the NFT and the money - Theoretically always true
     require(balanceOf(address(this), trades[trade_id].nft_id) >= 1);
-    require(balanceOf(address(this), 0) >= trades[trade_id].amount_paid_back);
+    require(balanceOf(address(this), 0) >= trades[trade_id].paid_back_amount);
 
     // Transfer the NFT and fungible tokens from the contract to the borrower and lender respectively
-    safeTransfer(address(this), trades[trade_id].borrower, trades[trade_id].nft_id, 1, "0x0");  
-    safeTransfer(address(this), trades[trade_id].lender, 0, amount, "0x0");  
+    safeTransferFrom(address(this), trades[trade_id].borrower, trades[trade_id].nft_id, 1, "0x0");  
+    safeTransferFrom(address(this), trades[trade_id].lender, 0, trades[trade_id].paid_back_amount, "0x0");  
 
     // Set trade to close state
     trades[trade_id].state = State.CLOSED;
@@ -128,25 +129,26 @@ contract DeFi is ERC1155Reciever{
     return true;
   }
 
-  function liquidateTrade(uint256 trade_id) public returns bool {
-    require(msg.sender == trades[trade_id].lender)
+  function liquidateTrade(uint256 trade_id) public returns (bool) {
+    require(msg.sender == trades[trade_id].lender);
 
     // Amount to be paid by the borrower to the lender
-    uint256 amount_to_be_paid = trades[trade_id].amount_borrowed * (2.7183 ** ((seconds_since_start/31556952) 
+    uint256 seconds_since_start = now - trades[trade_id].start_time;
+    uint256 amount_to_be_paid = trades[trade_id].borrowing_amount * (uint256(3) ** ((seconds_since_start/31556952) 
       * trades[trade_id].apy));
 
     // Condition for liquidity still not determined. For the purpose of initial testing, trade can be liquidated any time.
     // Later on we need to make so after x amount of interest is acrued, then the contract can be liquidated
 
-    bool can_be_liquidated = true
+    bool can_be_liquidated = true;
 
     if (can_be_liquidated) {
       // Makes sure the contract still has the NFT and the money - Theoretically always true
       require(balanceOf(address(this), trades[trade_id].nft_id) >= 1);
-      require(balanceOf(address(this), 0) >= trades[trade_id].amount_paid_back);
+      require(balanceOf(address(this), 0) >= trades[trade_id].paid_back_amount);
 
-      safeTransfer(address(this), trades[trade_id].lender, trades[trade_id].nft_id, 1, "0x0");  
-      safeTransfer(address(this), trades[trade_id].lender, 0, trades[trade_id].amount_paid_back, "0x0");  
+      safeTransferFrom(address(this), trades[trade_id].lender, trades[trade_id].nft_id, 1, "0x0");  
+      safeTransferFrom(address(this), trades[trade_id].lender, 0, trades[trade_id].paid_back_amount, "0x0");  
 
       emit LiquidatedTrade(trade_id);
     } else {
--- smart-contracts/contracts/Tokens.sol
+++ smart-contracts/contracts/Tokens.sol
@@ -9,9 +9,14 @@ contract Tokens is ERC1155 {
     uint256 public constant SWORD = 1; // Sword is an NFT
     uint256 public constant LUCKY_POTION = 2; // Lucky Potion is an NFT
 
+    // Total Supply
+    uint256 public EMERALDS_total_supply = 10**13;
+    uint256 public SWORD_total_supply = 5;
+    uint256 public LUCKY_POTION_total_supply = 2;
+
     constructor() public ERC1155("http://localhost:8000/item/{id}/") {
-        _mint(msg.sender, EMERALDS, 10**13, ""); // Value includes 8 decimals, thus total supply is currently 10^5
-        _mint(msg.sender, SWORD, 5, ""); // NFT token that has only 5 copies
-        _mint(msg.sender, LUCKY_POTION, 2, ""); // NFT token that has only 2 copies
+        _mint(msg.sender, EMERALDS, EMERALDS_total_supply, ""); // Value includes 8 decimals, thus total supply is currently 10^5
+        _mint(msg.sender, SWORD_total_supply, 5, ""); // NFT token that has only 5 copies
+        _mint(msg.sender, LUCKY_POTION_total_supply, 2, ""); // NFT token that has only 2 copies
     }
 }
