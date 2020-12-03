2ccfee69a59f9306aa53465058e78c0cd8e5215d
Preston Ong <preston4896@me.com>
2020-12-03 13:38:16 -0800

    DeFi.sol deployed successfully.

--- smart-contracts/migrations/2_deploy_contracts.js
+++ smart-contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Tokens = artifacts.require("Tokens");
+const DeFi = artifacts.require("DeFi");
 
 module.exports = function (deployer) {
   deployer.deploy(Tokens);
+  deployer.deploy(DeFi);
 };
