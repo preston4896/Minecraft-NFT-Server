f3ad31ab71a5fe7958dd8b797dc0332db040e200
Preston Ong <preston4896@me.com>
2020-12-23 11:11:36 +0800

    Checkpoint 1 - 12/23: Updated Governance.sol and test code.

--- smart-contracts/test/test_gov.test.js
+++ smart-contracts/test/test_gov.test.js
@@ -31,9 +31,9 @@ contract("Governance", (accounts) => {
         assert.equal(against_balance, 1);
     })
 
-    // TODO: Test description.
     it("2. Proposal creations.", async() => {
-        let description = new Array(140).fill(web3.utils.keccak256("Hello, world!"));
+        // let description = new Array(140).fill(web3.utils.keccak256("Hello, world!"));
+        let description = web3.utils.keccak256("Hello, world!");
 
         // an account attempts to create a proposal with insufficient governance token.
         try {
@@ -43,26 +43,24 @@ contract("Governance", (accounts) => {
         }
 
         let expected_startTime = Math.floor(Date.now()/1000);
-        let expected_endTime = expected_startTime + 120; // 2 minutes into the future
+        let expected_endTime = expected_startTime + 60; // 1 minute to vote
         await gov.createProposal(expected_endTime, description, {from: proposer});
 
         // verify proposal
         let proposal1_obj = await gov.proposals(0);
         // console.log(proposal1_obj);
 
-        // @mw2000 I can't assign bytes32 array from the parameter to the Proposal object.
         assert.equal(proposal1_obj.description, description, "Description should match.");
         assert.equal(proposal1_obj.end_date, expected_endTime, "End time should match.");
 
         expected_startTime = Math.floor(Date.now()/1000);
-        expected_endTime = expected_startTime + 120; // 2 minutes into the future
+        expected_endTime = expected_startTime + 60; // 1 minute to vote
         await gov.createProposal(expected_endTime, description, {from: proposer});
 
         // verify proposal
         let proposal2_obj = await gov.proposals(1);
 
-        // @mw2000 I can't assign bytes32 array from the parameter to the Proposal object.
-        // assert.equal(proposal1_obj.description, description, "Description should match.");
+        assert.equal(proposal1_obj.description, description, "Description should match.");
         assert.equal(proposal2_obj.end_date, expected_endTime, "End time should match.");
     })
 
@@ -80,8 +78,6 @@ contract("Governance", (accounts) => {
             assert(error.message.indexOf("revert") >= 0, "error message must contain revert.");
         }
 
-        // @mw2000 consider adding safety check to not vote non-existent proposals.
-
         await gov.voteForProposal(0, {from: voted_for});
         await gov.voteAgainstProposal(0, {from: voted_against});
         await gov.voteForProposal(1, {from: voted_against});
@@ -90,12 +86,16 @@ contract("Governance", (accounts) => {
         let pro_0 = await gov.proposals(0);
         let pro_1 = await gov.proposals(1);
 
-        // console.log(pro_0);
+        // verify vote count.
+        assert.equal(pro_0.votes_against, 1);
+        assert.equal(pro_0.votes_for, 1);
+        assert.equal(pro_1.votes_against, 1);
+        assert.equal(pro_1.votes_for, 1);
 
         // verify votes.
-        assert.equal(pro_0.votes_against[voted_against], 1);
-        assert.equal(pro_0.votes_for[voted_for], 1);
-        assert.equal(pro_1.votes_against[voted_for], 1);
-        assert.equal(pro_1.votes_for[voted_against], 1);
+        let voted_0_prop_0 = await gov.verify_votes(voted_for, 0);
+        let voted_1_prop_0 = await gov.verify_votes(voted_against, 0);
+        let voted_0_prop_1 = await gov.verify_votes(voted_for, 1);
+        let voted_1_prop_1 = await gov.verify_votes(voted_against, 1);
     })
 })
