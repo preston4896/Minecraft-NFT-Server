208372c9ed6daee8396d69a77c2cad53e03c2a28
Mihir <mwadekar2000@gmail.com>
2020-12-23 15:24:15 -0800

    Added comments to Token.sol

--- smart-contracts/contracts/Tokens.sol
+++ smart-contracts/contracts/Tokens.sol
@@ -8,22 +8,31 @@ contract Tokens is ERC1155 {
     uint256 token_ids;
 
     // Constant Token ids
-    uint256 public constant EMERALDS = 0;  // Emeralds is the fungible tokens to be used
-    uint256 public constant GOVERNANCE = 1; // Governance token of the project
+    // Emeralds is the fungible tokens to be used
+    uint256 public constant EMERALDS = 0;  
+     // Governance token of the project
+    uint256 public constant GOVERNANCE = 1;
 
     // Total Supply
     uint256 public EMERALDS_total_supply = 10**13;
     uint256 public GOVERNANCE_total_supply = 10**5;
 
     constructor() public ERC1155("http://localhost:8000/item/{id}/") {
-        _mint(msg.sender, EMERALDS, EMERALDS_total_supply, ""); // Minting Fungible token
-        _mint(msg.sender, GOVERNANCE, GOVERNANCE_total_supply, ""); // Minting Governance token
+        // Minting Fungible token
+        _mint(msg.sender, EMERALDS, EMERALDS_total_supply, ""); 
+        // Minting Governance token
+        _mint(msg.sender, GOVERNANCE, GOVERNANCE_total_supply, ""); 
+
         token_ids = 2;
     }
 
+    // Wrapper around the ERC 1155 minting function. Mints an NFT token and assigns an ID to it
     function mint(uint256 total_supply) returns (uint256) {
-        uint256 nft_id = token_ids;
-        _mint(msg.sender, nft_id, total_supply, "");
+        // Get the current available id
+        uint256 nft_id = token_ids; 
+        // Minting the token
+        _mint(msg.sender, nft_id, total_supply, ""); 
+
         token_ids++;
         return nft_id;
     }
