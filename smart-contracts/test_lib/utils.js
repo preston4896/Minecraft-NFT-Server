6a615e49f32bee88030c6ff7caa1fef88ec1822f
Preston Ong <preston4896@me.com>
2021-01-21 20:09:52 +0800

    Checkpoint 4 - 1/21 : updated utils.js

--- smart-contracts/test_lib/utils.js
+++ smart-contracts/test_lib/utils.js
@@ -4,96 +4,96 @@
 */
 
 advanceTime = (time) => {
-    return new Promise((resolve, reject) => {
-      web3.currentProvider.send({
-        jsonrpc: '2.0',
-        method: 'evm_increaseTime',
-        params: [time],
-        id: new Date().getTime()
-      }, (err, result) => {
-        if (err) { return reject(err) }
-        return resolve(result)
-      })
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [time],
+      id: new Date().getTime()
+    }, (err, result) => {
+      if (err) { return reject(err) }
+      return resolve(result)
     })
-  }
+  })
+}
   
-  advanceBlock = () => {
-    return new Promise((resolve, reject) => {
-      web3.currentProvider.send({
+advanceBlock = () => {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send({
+      jsonrpc: '2.0',
+      method: 'evm_mine',
+      id: new Date().getTime()
+    }, (err, result) => {
+      if (err) { return reject(err) }
+      return resolve(result)
+    })
+  })
+}
+  
+advanceBlockAndSetTime = (time) => {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send({
         jsonrpc: '2.0',
         method: 'evm_mine',
+        params: [time],
         id: new Date().getTime()
-      }, (err, result) => {
+    }, (err, result) => {
         if (err) { return reject(err) }
         return resolve(result)
-      })
     })
-  }
+  })
+}
   
-  advanceBlockAndSetTime = (time) => {
-      return new Promise((resolve, reject) => {
-          web3.currentProvider.send({
-              jsonrpc: '2.0',
-              method: 'evm_mine',
-              params: [time],
-              id: new Date().getTime()
-          }, (err, result) => {
-              if (err) { return reject(err) }
-              return resolve(result)
-          })
-      })
-  }
-  
-  advanceTimeAndBlock = async (time) => {
-      //capture current time
-      let block = await web3.eth.getBlock('latest')
-      let forwardTime = block['timestamp'] + time
-  
-      return new Promise((resolve, reject) => {
-        web3.currentProvider.send({
-          jsonrpc: '2.0',
-          method: 'evm_mine',
-          params: [forwardTime],
-          id: new Date().getTime()
-      }, (err, result) => {
-          if (err) { return reject(err) }
-          return resolve(result)
-      })
+advanceTimeAndBlock = async (time) => {
+  //capture current time
+  let block = await web3.eth.getBlock('latest')
+  let forwardTime = block['timestamp'] + time
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send({
+      jsonrpc: '2.0',
+      method: 'evm_mine',
+      params: [forwardTime],
+      id: new Date().getTime()
+    }, (err, result) => {
+      if (err) { return reject(err) }
+      return resolve(result)
     })
-  }
+  })
+}
   
-  takeSnapshot = () => {
-    return new Promise((resolve, reject) => {
-      web3.currentProvider.send({
-        jsonrpc: '2.0',
-        method: 'evm_snapshot',
-        id: new Date().getTime()
-      }, (err, snapshotId) => {
-        if (err) { return reject(err) }
-        return resolve(snapshotId)
-      })
+takeSnapshot = () => {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send({
+      jsonrpc: '2.0',
+      method: 'evm_snapshot',
+      id: new Date().getTime()
+    }, (err, snapshotId) => {
+      if (err) { return reject(err) }
+      return resolve(snapshotId)
     })
-  }
-  
-  revertToSnapshot = (id) => {
-    return new Promise((resolve, reject) => {
-      web3.currentProvider.send({
-        jsonrpc: '2.0',
-        method: 'evm_revert',
-        params: [id],
-        id: new Date().getTime()
-      }, (err, result) => {
-        if (err) { return reject(err) }
-        return resolve(result)
-      })
+  })
+}
+
+revertToSnapshot = (id) => {
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send({
+      jsonrpc: '2.0',
+      method: 'evm_revert',
+      params: [id],
+      id: new Date().getTime()
+    }, (err, result) => {
+      if (err) { return reject(err) }
+      return resolve(result)
     })
-  }
-  
-  module.exports = {
-    advanceTime,
-    advanceBlock,
-    advanceBlockAndSetTime,
-    advanceTimeAndBlock,
-    takeSnapshot,
-    revertToSnapshot
-  }
+  })
+}
+
+module.exports = {
+  advanceTime,
+  advanceBlock,
+  advanceBlockAndSetTime,
+  advanceTimeAndBlock,
+  takeSnapshot,
+  revertToSnapshot
+}
